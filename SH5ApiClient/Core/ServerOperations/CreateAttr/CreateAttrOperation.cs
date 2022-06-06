49a411879d1d815157503f547ae7d3f8d7cc49b0
Алексей Прошин <sin.06@mail.ru>
2022-06-06 14:55:38 +0200

    Добавлена операция добавления атриьутного поля.

---
+++ SH5ApiClient/Core/ServerOperations/CreateAttr/CreateAttrOperation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SH5ApiClient.Core.ServerOperations
+{
+    internal class CreateAttrOperation : OperationBase
+    {
+        public override string Uri => "sh5CreateAttr";
+    }
+}
