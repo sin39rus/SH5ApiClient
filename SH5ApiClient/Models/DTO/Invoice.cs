ca3d4ac3887c3e3408ac21590e5acd70ad056a5b
Алексей Прошин <sin.06@mail.ru>
2022-06-08 16:07:58 +0200

    Добавлен объект Счет-фактура

---
+++ SH5ApiClient/Models/DTO/Invoice.cs
@@ -0,0 +1,36 @@
+﻿namespace SH5ApiClient.Models.DTO
+{
+    /// <summary>Счет-фактура</summary>
+    public class Invoice
+    {
+        /// <summary>Rid</summary>
+        [OriginalName("1")]
+        public uint Rid { set; get; }
+
+        /// <summary>Type</summary>
+        [OriginalName("5")]
+        public uint? Type { set; get; } //ToDo: Возможно есть класификатор типов для счета фактуры.
+
+        /// <summary>Name</summary>
+        [OriginalName("3")]
+        public string? Name { set; get; }
+
+        /// <summary>Date</summary>
+        [OriginalName("31")]
+        public DateTime? Date { set; get; }
+
+
+        public static Invoice? Parse(Dictionary<string, string> value)
+        {
+            if(!value.Any())
+                return null;
+            return new Invoice
+            {
+                Rid = uint.TryParse(value["1"], out uint rid) ? rid : 0,
+                Type = uint.TryParse(value["5"], out uint type) ? type : null,
+                Name = value.GetValueOrDefault("3"),
+                Date = DateTime.TryParse(value.GetValueOrDefault("31"), out DateTime date) ? date : null
+            };
+        }
+    }
+}
