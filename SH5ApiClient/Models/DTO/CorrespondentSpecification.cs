1624c20cbbdaf69ef407ac3f8f2dbe8677fad8ed
Алексей Прошин <sin.06@mail.ru>
2022-06-08 16:08:18 +0200

    Добавлен объект Спецификация контрагента

---
+++ SH5ApiClient/Models/DTO/CorrespondentSpecification.cs
@@ -0,0 +1,25 @@
+﻿namespace SH5ApiClient.Models.DTO
+{
+    /// <summary>Спецификация контрагента</summary>
+    public class CorrespondentSpecification
+    {
+        /// <summary>Rid</summary>
+        [OriginalName("1")]
+        public int? Rid { set; get; }
+
+        /// <summary>Name</summary>
+        [OriginalName("3")]
+        public string? Name { set; get; }
+
+        public static CorrespondentSpecification? Parse(Dictionary<string, string> value)
+        {
+            if (!value.Any())
+                return null;
+            return new CorrespondentSpecification
+            {
+                Rid = int.TryParse(value.GetValueOrDefault("1"), out int rid) ? rid : null,
+                Name = value.GetValueOrDefault("3")
+            };
+        }
+    }
+}
