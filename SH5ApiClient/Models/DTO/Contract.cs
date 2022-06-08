4cf9b1aff67c0af205a0f6b8754415d2a5a49dee
Алексей Прошин <sin.06@mail.ru>
2022-06-08 16:37:14 +0200

    Добавлен объект Договор

---
+++ SH5ApiClient/Models/DTO/Contract.cs
@@ -0,0 +1,30 @@
+﻿namespace SH5ApiClient.Models.DTO
+{
+    /// <summary>Договор</summary>
+    public class Contract
+    {
+        /// <summary>Rid</summary>
+        [OriginalName("1")]
+        public int? Rid { set; get; }
+
+        /// <summary>Name</summary>
+        [OriginalName("3")]
+        public string? Name { set; get; }
+
+        /// <summary>Date</summary>
+        [OriginalName("31")]
+        public DateTime? Date { set; get; }
+
+        public static Contract? Parse(Dictionary<string, string> value)
+        {
+            if (!value.Any())
+                return null;
+            return new Contract
+            {
+                Rid = int.TryParse(value.GetValueOrDefault("1"), out int rid) ? rid : null,
+                Name = value.GetValueOrDefault("3"),
+                Date = DateTime.TryParse(value.GetValueOrDefault("31"), out DateTime date) ? date : null
+            };
+        }
+    }
+}
