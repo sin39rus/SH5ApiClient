4126bad4eb22db96c7376452f710b089ff2bfc0d
Алексей Прошин <sin.06@mail.ru>
2022-06-08 16:22:32 +0200

    Добавлен объект Бух Операция.

---
+++ SH5ApiClient/Models/DTO/BuhOperation.cs
@@ -0,0 +1,26 @@
+﻿namespace SH5ApiClient.Models.DTO
+{
+    /// <summary>Бух. операция</summary>
+    [OriginalName("179")]
+    public class BuhOperation
+    {
+        /// <summary>Rid</summary>
+        [OriginalName("1")]
+        public int? Rid { set; get; }
+
+        /// <summary>Name</summary>
+        [OriginalName("3")]
+        public string? Name { set; get; }
+
+        public static BuhOperation? Parse(Dictionary<string, string> value)
+        {
+            if (!value.Any())
+                return null;
+            return new BuhOperation
+            {
+                Rid = int.TryParse(value.GetValueOrDefault("1"), out int rid) ? rid : null,
+                Name = value.GetValueOrDefault("3")
+            };
+        }
+    }
+}
