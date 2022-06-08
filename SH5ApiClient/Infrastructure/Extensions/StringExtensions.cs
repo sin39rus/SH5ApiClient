0bbbbbbba861b9919d7cdf1038764e088b23a61a
Алексей Прошин <sin.06@mail.ru>
2022-06-08 16:09:05 +0200

    Добавлено расширение, отрезающее строку в начале и в конце

--- SH5ApiClient/Infrastructure/Extensions/StringExtensions.cs
+++ SH5ApiClient/Infrastructure/Extensions/StringExtensions.cs
@@ -27,5 +27,29 @@ namespace SH5ApiClient.Infrastructure.Extensions
                 return false;
             return true;
         }
+        public static string TrimStart(this string target, string trimString)
+        {
+            if (string.IsNullOrEmpty(trimString)) return target;
+
+            string result = target;
+            while (result.StartsWith(trimString))
+            {
+                result = result[trimString.Length..];
+            }
+
+            return result;
+        }
+        public static string TrimEnd(this string target, string trimString)
+        {
+            if (string.IsNullOrEmpty(trimString)) return target;
+
+            string result = target;
+            while (result.EndsWith(trimString))
+            {
+                result = result[..^trimString.Length];
+            }
+
+            return result;
+        }
     }
 }
