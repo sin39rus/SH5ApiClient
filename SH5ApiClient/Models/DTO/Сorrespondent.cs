7aa815c76b7f0f8440f11355f022b4f3b62fb04a
Алексей Прошин <sin.06@mail.ru>
2022-06-08 16:09:53 +0200

    Добавлено поле спецификации

--- SH5ApiClient/Models/DTO/Сorrespondent.cs
+++ SH5ApiClient/Models/DTO/Сorrespondent.cs
@@ -42,28 +42,37 @@
         /// <summary>Атрибуты типа 34</summary>
         [OriginalName("34")]
         public Dictionary<string, string> Attributes34 { set; get; } = new();
+
+        [OriginalName("114")]
+        public CorrespondentSpecification? CorrespondentSpecification { set; get; }
+
         internal static IEnumerable<Сorrespondent> GetСorrespondentsFromSHAnswear(ExecOperationContent answear)
         {
             foreach (Dictionary<string, string>? value in answear.GetValues())
             {
-                yield return Parse(value);
+                var correspondent = Parse(value);
+                if (correspondent is not null)
+                    yield return correspondent;
             }
         }
 
-        public static Сorrespondent Parse(Dictionary<string, string> value)
+        public static Сorrespondent? Parse(Dictionary<string, string> value)
         {
+            if (!value.Any())
+                return null;
             return new Сorrespondent
             {
                 Rid = int.TryParse(value["1"], out int rid) ? rid : 0,
-                INN = value["2"],
-                Name = value["3"],
-                GUID = value["4"]?.TrimStart('{').TrimEnd('}'),
+                INN = value.GetValueOrDefault("2"),
+                Name = value.GetValueOrDefault("3"),
+                GUID = value.GetValueOrDefault("4")?.TrimStart('{').TrimEnd('}'),
                 CorrType = Enum.TryParse(typeof(CorrType), value.GetValueOrDefault("5"), out object? corrType) ? (CorrType?)corrType : null,
                 CorrTypeEx = Enum.TryParse(typeof(CorrTypeEx), value.GetValueOrDefault("32"), out object? corrTypeEx) ? (CorrTypeEx?)corrTypeEx : null,
                 SubType = Enum.TryParse(typeof(CorrType3), value.GetValueOrDefault("31"), out object? subType) ? (CorrType3?)subType : null,
-                Attributes6 = value.Where(t => t.Key.StartsWith("6\\")).ToDictionary(t => t.Key.TrimStart("6\\".ToCharArray()), g => g.Value),
-                Attributes7 = value.Where(t => t.Key.StartsWith("7\\")).ToDictionary(t => t.Key.TrimStart("7\\".ToCharArray()), g => g.Value),
-                Attributes34 = value.Where(t => t.Key.StartsWith("34\\")).ToDictionary(t => t.Key.TrimStart("34\\".ToCharArray()), g => g.Value)
+                Attributes6 = value.Where(t => t.Key.StartsWith("6\\")).ToDictionary(t => t.Key.TrimStart("6\\"), g => g.Value),
+                Attributes7 = value.Where(t => t.Key.StartsWith("7\\")).ToDictionary(t => t.Key.TrimStart("7\\"), g => g.Value),
+                Attributes34 = value.Where(t => t.Key.StartsWith("34\\")).ToDictionary(t => t.Key.TrimStart("34\\"), g => g.Value),
+                CorrespondentSpecification = CorrespondentSpecification.Parse(value.Where(t => t.Key.StartsWith("114\\")).ToDictionary(t => t.Key.TrimStart("114\\"), g => g.Value))
             };
         }
 
