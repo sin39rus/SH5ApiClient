4101febb7be554f116102086cad48a73ccabce82
Алексей Прошин <sin.06@mail.ru>
2022-06-09 12:14:16 +0200

    В договор добавлены дополнительные поля.

--- SH5ApiClient/Models/DTO/Contract.cs
+++ SH5ApiClient/Models/DTO/Contract.cs
@@ -6,7 +6,15 @@
     {
         /// <summary>Rid</summary>
         [OriginalName("1")]
-        public int? Rid { set; get; }
+        public uint? Rid { set; get; }
+
+        /// <summary>Type</summary>
+        [OriginalName("5")]
+        public ushort? Type { set; get; }
+
+        /// <summary>Options</summary>
+        [OriginalName("33")]
+        public ushort? Options { set; get; }
 
         /// <summary>Name</summary>
         [OriginalName("3")]
@@ -16,15 +24,28 @@
         [OriginalName("31")]
         public DateTime? Date { set; get; }
 
+        /// <summary>PLimit</summary>
+        [OriginalName("51")]
+        public ushort? PLimit { set; get; }
+
+        /// <summary>PDow</summary>
+        [OriginalName("52")]
+        public ushort? PDow { set; get; }
+
+
         public static Contract? Parse(Dictionary<string, string> value)
         {
             if (!value.Any())
                 return null;
             return new Contract
             {
-                Rid = int.TryParse(value.GetValueOrDefault("1"), out int rid) ? rid : null,
+                Rid = uint.TryParse(value.GetValueOrDefault("1"), out uint rid) ? rid : null,
+                Type = ushort.TryParse(value.GetValueOrDefault("5"), out ushort type) ? type : null,
+                Options = ushort.TryParse(value.GetValueOrDefault("33"), out ushort options) ? options : null,
                 Name = value.GetValueOrDefault("3"),
-                Date = DateTime.TryParse(value.GetValueOrDefault("31"), out DateTime date) ? date : null
+                Date = DateTime.TryParse(value.GetValueOrDefault("31"), out DateTime date) ? date : null,
+                PLimit = ushort.TryParse(value.GetValueOrDefault("51"), out ushort pLimit) ? pLimit : null,
+                PDow = ushort.TryParse(value.GetValueOrDefault("52"), out ushort pDow) ? pDow : null,
             };
         }
     }
