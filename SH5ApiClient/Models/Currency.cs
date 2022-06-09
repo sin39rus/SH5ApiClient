fe5aa3f2c3110a234141df120f404dc0e57a6ffb
Алексей Прошин <sin.06@mail.ru>
2022-06-09 12:25:55 +0200

    Добавлен метод Parse

--- SH5ApiClient/Models/Currency.cs
+++ SH5ApiClient/Models/Currency.cs
@@ -1,11 +1,31 @@
 ﻿namespace SH5ApiClient.Models
 {
-
+    /// <summary>Валюта</summary>
+    [OriginalName("100")]
     public class Currency
     {
         [OriginalName("1")]
-        public int Rid { get; set; }
+        public uint? Rid { get; set; }
+
+
+        [OriginalName("2")]
+        public string? Code { get; set; }
+
+
         [OriginalName("3")]
         public string? Name { get; set; }
+
+
+        public static Currency? Parse(Dictionary<string, string> value)
+        {
+            if (!value.Any())
+                return null;
+            return new Currency
+            {
+                Rid = uint.TryParse(value["1"], out uint rid) ? rid : null,
+                Code = value.GetValueOrDefault("2"),
+                Name = value.GetValueOrDefault("3")
+            };
+        }
     }
 }
