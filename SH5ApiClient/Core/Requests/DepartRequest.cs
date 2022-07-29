5faabca31aed2bb6fd4b3a512f38c571e5669f6e
Алексей Прошин <sin.06@mail.ru>
2022-07-29 10:45:37 +0200

    Добавлены запросы: списка подразделений, подразделения.

---
+++ SH5ApiClient/Core/Requests/DepartRequest.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SH5ApiClient.Core.Requests
+{
+    public class DepartRequest : RequestBase
+    {
+        private const string procName = "Depart";
+        public override OperationBase Operation => new ExecOperation();
+        private uint _rid;
+        private string _guid;
+        public DepartRequest(ConnectionParamSH5 connectionParamSH5, uint rid, string guid) : base(procName, connectionParamSH5)
+        {
+            _rid = rid;
+            _guid = guid;
+        }
+        public override string CreateJsonRequest()
+        {
+            return new JObject(
+                new JProperty("UserName", UserName),
+                new JProperty("Password", Password),
+                new JProperty("procName", ProcName),
+                new JProperty("Input", new JArray(
+                    new JObject(
+                        new JProperty("head", "106"),
+                        new JProperty("original", new JArray("1", "4")),
+                        new JProperty("values", new JArray(
+                            new JArray(_rid),
+                            new JArray("{" + _guid + "}"))))))).ToString();
+        }
+    }
+}
---
+++ SH5ApiClient/Core/Requests/DepartsRequest.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace SH5ApiClient.Core.Requests
+{
+    public class DepartsRequest : RequestBase
+    {
+        private const string procName = "Departs";
+        public override OperationBase Operation => new ExecOperation();
+        public DepartsRequest(ConnectionParamSH5 connectionParamSH5) : base(procName, connectionParamSH5)
+        {
+
+        }
+        public override string CreateJsonRequest() =>
+            JsonConvert.SerializeObject(this);
+    }
+}
