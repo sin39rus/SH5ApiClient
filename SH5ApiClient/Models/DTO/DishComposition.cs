96a81c797f65b38144d45e571b9d8bb3c0d5911b
Алексей Прошин <sin.06@mail.ru>
2022-09-23 16:48:19 +0200

    Добавлены объекты: Комплект и Версия комплетка

---
+++ SH5ApiClient/Models/DTO/DishComposition.cs
@@ -0,0 +1,30 @@
+﻿namespace SH5ApiClient.Models.DTO
+{
+    /// <summary>Комплект</summary>
+    [OriginalName("215")]
+    public class DishComposition
+    {
+        /// <summary>Rid</summary>
+        [OriginalName("1")]
+        public uint? Rid { set; get; }
+
+        /// <summary>Name</summary>
+        [OriginalName("3")]
+        public string? Name { set; get; }
+
+        /// <summary>Версия комплекта</summary>
+        [OriginalName("216")]
+        public DishCompositionVersion? DishCompositionVersion { get; set; }
+        public static DishComposition? Parse(Dictionary<string, string> value)
+        {
+            if (!value.Any())
+                return null;
+            return new DishComposition
+            {
+                Rid = uint.TryParse(value["1"], out uint rid) ? rid : null,
+                Name = value.GetValueOrDefault("3"),
+                DishCompositionVersion = DishCompositionVersion.Parse(value.Where(t => t.Key.StartsWith("216\\")).ToDictionary(t => t.Key.TrimStart("216\\"), g => g.Value)),
+            };
+        }
+    }
+}
---
+++ SH5ApiClient/Models/DTO/DishCompositionVersion.cs
@@ -0,0 +1,32 @@
+﻿namespace SH5ApiClient.Models.DTO
+{
+    /// <summary>Версия комплекта</summary>
+    [OriginalName("216")]
+    public class DishCompositionVersion
+    {
+        /// <summary>Rid</summary>
+        [OriginalName("1")]
+        public uint? Rid { set; get; }
+
+        /// <summary>Name</summary>
+        [OriginalName("2")]
+        public ushort? Version { set; get; }
+
+        /// <summary>Name</summary>
+        [OriginalName("3")]
+        public string? Name { set; get; }
+
+
+        public static DishCompositionVersion? Parse(Dictionary<string, string> value)
+        {
+            if (!value.Any())
+                return null;
+            return new DishCompositionVersion
+            {
+                Rid = uint.TryParse(value["1"], out uint rid) ? rid : null,
+                Version = ushort.TryParse(value["2"], out ushort version) ? version : null,
+                Name = value.GetValueOrDefault("3")
+            };
+        }
+    }
+}
