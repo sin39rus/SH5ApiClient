e9068bc81b30a3ffca34c54d1f20f090edd6cc87
Alexey Proshin <34774740+sin39rus@users.noreply.github.com>
2024-12-31 10:07:56 +0200

    Исправлена ошибка запроса поставщиков

--- SH5ApiClient/Core/Requests/CorrsRequest.cs
+++ SH5ApiClient/Core/Requests/CorrsRequest.cs
@@ -8,7 +8,7 @@ namespace SH5ApiClient.Core.Requests
     public class CorrsRequest : RequestBase
     {
         //Имя процедуры
-        private const string procName = "Corrs2";
+        private const string procName = "Corrs";
         public CorrsRequest(ConnectionParamSH5 connectionParam) : base(procName, connectionParam)
         {
 
